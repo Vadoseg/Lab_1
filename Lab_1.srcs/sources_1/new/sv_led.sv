84c3418b060af45d720729effb30bc5f9a375891
Vadoseg <145899163+Vadoseg@users.noreply.github.com>
2024-04-09 23:00:31 +0700

    Добавил второй  LED
    *Добавил с помощью массива
    *Изменил в большом модуле кол-во проводов([7:0]), а в маленьком оставил([3:0])
    Добавил пины в pinouts и укоротил там код при помощи -dict

--- Lab_1.srcs/sources_1/new/sv_led.sv
+++ Lab_1.srcs/sources_1/new/sv_led.sv
@@ -26,9 +26,8 @@ module sv_led#(
 (
     input i_clk,
     input i_rst,
-    output logic [3:0] o_led = '0
+    output logic [3:0] o_led
 );
-
 
 //Constants
     localparam COUNTER_PERIOD = int'(BLINK_PERIOD * CLK_FREQUENCY);
@@ -59,20 +58,68 @@ module sv_led#(
             led_on <= 1;
         end;
         o_led[3:1]<={$size(o_led[3:1]){led_on}}; // 3 wires will be equal to led_on, except first
-        
-        //o_led[3:1]<=logic'(led_on);
-        o_led[0]<=!led_on;  
+        o_led[0]<=!led_on;
     end
     
     
 endmodule
 
-module Led_top(
+
+/*module sv_led_2#(
+    parameter CLK_FREQUENCY = 200.0e6,
+    parameter BLINK_PERIOD = 1.0
+)
+(
+    input i_clk,
+    input i_rst,
+    output logic [7:4] o_led_2
+);
+
+//Constants
+    localparam COUNTER_PERIOD = int'(BLINK_PERIOD * CLK_FREQUENCY);
+    
+    localparam COUNTER_WIDTH  = int'($ceil($clog2(COUNTER_PERIOD+1)));
+    
+//Counter & Comparator
+    reg [COUNTER_WIDTH -1 : 0] counter_value ='0;
+    logic led_on;
+    
+    always_ff@(posedge i_clk)begin
+        if(i_rst || counter_value == COUNTER_PERIOD-1)
+        begin
+            counter_value <= 0;
+        end
+        
+        else begin
+            counter_value <= counter_value + 1;
+        end;
+    end
+    
+    always_ff@(posedge i_clk)begin
+        if(counter_value < COUNTER_PERIOD/2)
+        begin
+            led_on <= 0;
+        end
+        else begin
+            led_on <= 1;
+        end;
+        o_led_2[7:5]<={$size(o_led_2[7:5]){led_on}}; // 3 wires will be equal to led_on, except first
+        o_led_2[4]<=!led_on;  
+    end
+
+endmodule*/
+
+module Led_top#(
+    parameter CLK_FREQUENCY = 200.0e6, // Hz
+    parameter BLINK_PERIOD = 1.0 // Seconds
+)
+
+(
     input clk_ini_p,
     input clk_ini_n,
-    input i_rst,
-    output logic [3:0] o_led
-
+    input [1:0] i_rst,
+    output logic [7:0] o_led
+    //output logic [7:4] o_led_2
 );
 
 
@@ -91,11 +138,20 @@ module Led_top(
       .I(clk_out1)  // 1-bit input: Clock input
    );
     
-    sv_led LED (
+    sv_led#(
+        .CLK_FREQUENCY(CLK_FREQUENCY),
+        .BLINK_PERIOD(BLINK_PERIOD)
+    ) LED [1:0](
         .i_clk(i_clk),
         .i_rst(i_rst),
         .o_led(o_led)
     );
+    
+    /*sv_led_2 LED_2 (
+        .i_clk(i_clk),
+        .i_rst(i_rst[1]),
+        .o_led_2(o_led_2)
+    );*/
 endmodule
 
 
