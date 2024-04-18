5557c799bbff387851082a40b991d4ebc7925b89
Vadoseg <145899163+Vadoseg@users.noreply.github.com>
2024-04-18 17:26:44 +0700

    Связал все модули для l4 в топ модуле

--- Lab_1.srcs/sources_1/new/l4_top.sv
+++ Lab_1.srcs/sources_1/new/l4_top.sv
@@ -33,18 +33,20 @@ module l4_top#(
     int G_BYT = 1
 )(
     
-    input i_rst, //Maybe not in use because every module has its own reset
+    input i_rst_src, //Maybe not in use because every module has its own reset
+    input i_rst_sink,
+    input i_rst_fifo,
     input i_clk,
     
-    output data
+    output o_top_data
     );
     
     if_axis #(.N(G_BYT)) s_axis ();
     if_axis #(.N(G_BYT)) m_axis ();
     
     l4_source SOURCE(
-        .i_reset    (i_rst),
-        .i_clk      (i_clk),
+        .i_reset    (i_rst_src),
+        .i_clk      (i_clk    ),
         
         .src_tvalid (src_fifo_tvalid),
         .src_tdata  (src_fifo_tdata ),
@@ -53,18 +55,18 @@ module l4_top#(
     
     axis_data_fifo_0 AXIS (
         
-        .s_axis_aresetn      (i_aresetn),
-        .s_axis_aclk         (i_aclk   ), 
+        .s_axis_aresetn      (i_rst_fifo),
+        .s_axis_aclk         (i_clk     ), //Maybe need its own clock
                                            
-        .s_axis_tready      (s_axis.tready),
-        .s_axis_tvalid      (src_fifo_tvalid), // ПОМЕНЯТЬ
-        .s_axis_tlast       (s_axis.tlast ),
-        .s_axis_tdata       (s_axis.tdata ),
+        .s_axis_tready      (s_axis.tready   ),
+        .s_axis_tvalid      (src_fifo_tvalid ),
+        .s_axis_tlast       (src_fifo_tlast  ),
+        .s_axis_tdata       (src_fifo_tdata  ),
                                            
-        .m_axis_tready      (m_axis.tready),
-        .m_axis_tvalid      (m_axis.tvalid),
-        .m_axis_tlast       (m_axis.tlast ),
-        .m_axis_tdata       (m_axis.tdata ),
+        .m_axis_tready      (m_axis.tready   ),
+        .m_axis_tvalid      (fifo_sink_tvalid),
+        .m_axis_tlast       (fifo_sink_tlast ),
+        .m_axis_tdata       (fifo_sink_tdata ),
                                            
         .axis_wr_data_count (             ),
         .axis_rd_data_count (             ),
@@ -74,12 +76,13 @@ module l4_top#(
     );
     
     l4_sink SINK(
-        .i_reset    (i_rst),
-        .i_clk      (i_clk),
+        .i_reset    (i_rst_sink),
+        .i_clk      (i_clk     ),
         
         .sink_tvalid (fifo_sink_tvalid),
         .sink_tdata  (fifo_sink_tdata ),
-        .sink_tlast  (fifo_sink_tlast )
-    
+        .sink_tlast  (fifo_sink_tlast ),
+        
+        .sink_data   (o_top_data      )
     );
 endmodule
