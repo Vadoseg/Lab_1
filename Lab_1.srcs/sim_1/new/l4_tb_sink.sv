52dc4caf651fb0282ddda6e5051bd3bb3024dfbd
Vadoseg <145899163+Vadoseg@users.noreply.github.com>
2024-05-03 13:43:52 +0700

    Сделал логику для модуля sink и tb для него
    * Всё работает корректно

---
+++ Lab_1.srcs/sim_1/new/l4_tb_sink.sv
@@ -0,0 +1,89 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/03/2024 11:08:57 AM
+// Design Name: 
+// Module Name: l4_tb_sink
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module l4_tb_sink#(
+        parameter int G_BYT = 1,
+        parameter int G_BIT_WIDTH = 8 * G_BYT
+    )(
+    
+    );
+    
+    bit i_clk = '0;
+    bit i_sink_tvalid  = '0;
+    bit i_sink_tlast = '0;
+    logic [G_BIT_WIDTH-1:0] i_sink_tdata = '0;
+    
+    localparam T_CLK = 1e9 / 1e9;
+
+    l4_sink#( 
+        .G_BYT       (G_BYT      ),
+        .G_BIT_WIDTH (G_BIT_WIDTH)
+    ) SINK (
+        .i_clk (i_clk),
+        .i_rst ('0   ), 
+        .i_sink_tvalid  (i_sink_tvalid),
+        .i_sink_tdata   (i_sink_tdata ),
+        .i_sink_tlast   (i_sink_tlast )
+    );
+    
+    always#(T_CLK * 10) i_clk = ~i_clk;
+
+
+    initial
+    begin
+    // States:
+    // S0: Init
+
+    // S1
+    /*i_sink_tvalid = '1;  // Header is not coming in
+    i_sink_tdata  = 72;
+    #(T_CLK * 20)
+    i_sink_tvalid = '0;*/
+    
+    // S2
+    #(T_CLK * 20)
+    i_sink_tvalid = '1;
+    i_sink_tdata = 10;
+    #(T_CLK * 20)
+
+    // S3
+    #(T_CLK * 20)
+    for (int i = 1; i < 11; i++) begin
+        i_sink_tdata = i;
+        #(T_CLK * 20);
+    end
+    i_sink_tvalid = '0;
+
+    // S4: Pause
+
+    // S5
+    #(T_CLK * 20)
+    i_sink_tvalid = '1;
+    i_sink_tdata  = 'h5B;
+    i_sink_tlast  = '1;
+    #(T_CLK * 20)
+    i_sink_tvalid = '0;
+    i_sink_tlast  = '0;
+    i_sink_tdata  = '0;
+    end
+
+endmodule
--- Lab_1.srcs/sources_1/new/l4_sink.sv
+++ Lab_1.srcs/sources_1/new/l4_sink.sv
@@ -35,19 +35,24 @@ module l4_sink#(
     input logic [G_BIT_WIDTH-1:0] i_sink_tdata,
     input logic i_sink_tlast,
     
-    output logic o_sink_good,
-    output logic o_sink_error,
+    output bit o_sink_good,
+    output bit o_sink_error,
     output o_sink_ready
     );
     
     logic q_clear = '0; 
-    logic /*[]*/ q_data_cnt = '0;
+    logic [4:0] q_data_cnt = '0;  // CHANGE BUS
     logic [G_BIT_WIDTH-1:0] q_crc_tdata = '0;
-    logic Length = '0;
+    logic [4:0] Length = '0;  // CHANGE BUS
 
     logic m_crc_valid ;
     logic [G_BIT_WIDTH-1:0] m_crc_data ;
 
+    localparam int C_IDLE_MAX  = 25;
+    localparam int C_IDLE_WIDTH  = ($ceil($clog2(C_IDLE_MAX+1)));
+
+    logic [C_IDLE_WIDTH-1:0]  q_idle_cnt  = '0;
+    
     typedef enum{
         S0 = 0,     // Ready/Init
         S1 = 1,     // Header
@@ -66,27 +71,33 @@ module l4_sink#(
             S0: begin
                 q_crnt_state <= S1;
                 q_data_cnt <= 1;
+                o_sink_good <= '0;
+                o_sink_error <= 0;
             end
             S1: begin 
                 if(i_sink_tvalid) begin
-                    q_crc_tdata <= i_sink_tdata;   // Header
-                    q_crnt_state <= S2;
+                    q_crc_tdata <= i_sink_tdata;   // Header is not coming  in
+                    q_crnt_state <= S2;     // If we will send header then we need to exit from number 72 bcs in next state it still will be 72
                 end
             end
             S2: begin 
                 if(i_sink_tvalid) begin
                     Length <= i_sink_tdata;   // Length
+                    q_crc_tdata <= i_sink_tdata;
+                    q_crc_tdata <= q_data_cnt;  // Needed bcs Length was counting twice in the next state (Now q_crc_tdata <= 1)
+                    //q_data_cnt <= q_data_cnt + 1;
                     q_crnt_state <= S3;
                 end
             end
             S3: begin
                 if(i_sink_tvalid) begin
-                    q_crc_tdata <= i_sink_tdata;
+                    q_crc_tdata <= i_sink_tdata + 1;  // +1 bcs i_sink_data need to be on 1 further (bcs in past state we alredy added 1)
                     q_data_cnt <= q_data_cnt + 1;   // Length
                     
-                    if (q_data_cnt == Length + 1) begin
+                    if (q_data_cnt == Length) begin
                         q_crnt_state <= S4;
                         q_data_cnt <= 1;
+                        q_crc_tdata <= '0;  // Needed for correct visualisation and bcs we need to reset to zero
                     end
                 end
             end
@@ -102,10 +113,18 @@ module l4_sink#(
                     else if (i_sink_tlast && (m_crc_data != i_sink_tdata)) begin
                         o_sink_error <= '1;
                     end
-                    o_sink_good <= '0;
-                    o_sink_error <= '0;
+                    q_crnt_state <= S6;
                 end
             end
+            S6: begin
+                o_sink_good <= '0;
+                o_sink_error <= '0;
+                q_crnt_state <= (q_idle_cnt == C_IDLE_MAX-1) ? S0 : S6;
+                q_idle_cnt <= q_idle_cnt+1;
+                q_clear <= '0;
+            end
+            default:
+                q_crnt_state <= S0;
         endcase
         
         if (i_rst)
@@ -130,9 +149,9 @@ module l4_sink#(
 		.i_crc_s_rst_p (q_clear), // Sync Reset, Active High. Reset CRC To Initial Value.
 		.i_crc_ini_vld ('0     ), // Input Initial Valid
 		.i_crc_ini_dat ('0     ), // Input Initial Value
-		.i_crc_wrd_vld (i_sink_tvalid && (q_crnt_state != S1)), // Word Data Valid Flag 
+		.i_crc_wrd_vld (i_sink_tvalid && (q_crnt_state != S1) && (q_crnt_state != S5)), // Word Data Valid Flag 
 		.o_crc_wrd_rdy (o_sink_ready ), // Ready To Recieve Word Data
-		.i_crc_wrd_dat (q_crc_tdata ), // Word Data
+		.i_crc_wrd_dat (/*i_sink_tdata*/ q_crc_tdata ), // Word Data
 		.o_crc_res_vld (m_crc_valid), // Output Flag of Validity, Active High for Each WORD_COUNT Number
 		.o_crc_res_dat (m_crc_data )  // Output CRC from Each Input Word
     );
