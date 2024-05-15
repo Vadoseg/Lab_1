93fe7ab3d5cc8a103e83a08e7db6bbc0fe79abbb
Vadoseg <145899163+Vadoseg@users.noreply.github.com>
2024-05-15 19:07:16 +0700

    Оптимизировал работу source, заработал top
    * Оптимизировал работу l4_if_source и добился правильной работы в l4_if_top
    * Требуется найти способ избавиться от Pause State (S2)
    * Дальше попробую оптимизировать работу l4_if_sink

--- Lab_1.srcs/sources_1/new/l4_if_source.sv
+++ Lab_1.srcs/sources_1/new/l4_if_source.sv
@@ -19,31 +19,26 @@ module l4_if_source#(
     );
     
     typedef enum{
-        S0 = 0,     // Ready/Init && Header
-        S1 = 1,     // Length
-        S2 = 2,     // Payload
-        S3 = 3      // CRC_DATA  
+        S0 = 0,     // Length && Header
+        S1 = 1,     // Payload
+        S2 = 2,     // Pause
+        S3 = 3      // CRC_DATA
     } t_fsm_states;
     
     t_fsm_states q_crnt_state = S0;
     
 // Interface init
-    
     initial begin           // No m_axis.tready bcs its input
         m_axis.tvalid = '0;
         m_axis.tdata  = '0;
         m_axis.tlast  = '0;
     end
 
-// Local constants    
+// Local length buffer   
     logic [G_CNT_WIDTH-1:0] buf_length = '0; // Needed bcs we need to remember last input on i_length
     
-    localparam int C_IDLE_MAX  = 25;
-    localparam int C_IDLE_WIDTH  = ($ceil($clog2(C_IDLE_MAX+1)));
-    
-// Making counts for states of FSM  
-    logic [G_CNT_WIDTH-1:0]  q_data_cnt  = '0;
-    logic [C_IDLE_WIDTH-1:0]  q_idle_cnt  = '0;
+// Making counts for FSM  
+    logic [G_CNT_WIDTH-1:0]  q_data_cnt = 1;
 
 // For CRC 
     logic m_crc_valid;
@@ -56,43 +51,44 @@ module l4_if_source#(
         
         case(q_crnt_state)
             S0: begin
-                q_idle_cnt <= '0;
-                q_data_cnt <= 1;
                 m_axis.tdata <= 72;
-                q_crnt_state <= (m_axis.tready) ? S1 : S0;
-            end
-            S1: begin
-                m_axis.tdata <= buf_length;
-                m_axis.tvalid  <= '1;
+
+                if (m_axis.tready && m_axis.tdata == 72) begin
+                    m_axis.tdata <= buf_length;
+                    m_axis.tvalid  <= '1;
+                end
+
                 if (m_axis.tready && m_axis.tvalid) begin
-                    q_crnt_state <= S2;
+                    q_crnt_state  <= S1;
                     m_axis.tvalid <= '0;
-                    m_axis.tdata <= q_data_cnt;
-                    q_data_cnt <= q_data_cnt + 1;
+                    m_axis.tdata  <= q_data_cnt;
+                    q_data_cnt    <= q_data_cnt + 1;
                 end
             end
-            S2: begin
+            S1: begin
                 m_axis.tvalid  <= '1;
                 if (m_axis.tready && m_axis.tvalid) begin
                     m_axis.tdata <= q_data_cnt;
-                    q_data_cnt <= q_data_cnt + 1; 
+                    q_data_cnt   <= q_data_cnt + 1; 
                     
                     if (q_data_cnt == buf_length + 1) begin
-                        q_crnt_state <= S3;
-                        q_data_cnt <= 1;
+                        q_crnt_state <= S2;
+                        q_data_cnt   <= 1;
                         m_axis.tvalid <= '0; 
                     end
                 end
-            end    
-            S3: begin                
+            end  
+            S2: begin
+                q_crnt_state = S3;  // Need bcs m_crc_data will be still equal to 0xC2 instead of 0x5B  (Found a way to optimizate this)
+            end  
+            S3: begin            
                 m_axis.tvalid  <= '1;
-                m_axis.tlast <= '1;
-                m_axis.tdata <= m_crc_data;
-                
+                m_axis.tlast   <= '1;
+                m_axis.tdata   <= m_crc_data;
                 if (m_axis.tready && m_axis.tvalid) begin
-                    q_crnt_state <= S0;
-                    m_axis.tvalid  <= '0;
-                    m_axis.tlast <= '0;
+                    m_axis.tlast    <= '0;
+                    q_crnt_state    <= S0;
+                    m_axis.tvalid   <= '0;
                 end            
             end
             
@@ -100,32 +96,37 @@ module l4_if_source#(
                 q_crnt_state <= S0;
         endcase;
         
-        if (i_rst)
-            q_crnt_state <= S0;
+        if (i_rst) begin
+            q_crnt_state    <= S0;
+            m_axis.tvalid   <= '0;
+            m_axis.tlast    <= '0;
+            m_axis.tdata    <= '0;
+            q_data_cnt      <=  1;
+        end
     end
 
 //Creating CRC-module inside Source
     crc#(
-        .POLY_WIDTH (G_BIT_WIDTH   ), // Size of The Polynomial Vector
-		.WORD_WIDTH (G_BIT_WIDTH   ), // Size of The Input Words Vector
-		.WORD_COUNT (0   ), // Number of Words To Calculate CRC, 0 - Always Calculate CRC On Every Input Word
-		.POLYNOMIAL ('hD5), // Polynomial Bit Vector
-		.INIT_VALUE ('1  ), // Initial Value
-		.CRC_REF_IN ('0  ), // Beginning and Direction of Calculations: 0 - Starting With MSB-First; 1 - Starting With LSB-First
-		.CRC_REFOUT ('0  ), // Determines Whether The Inverted Order of The Bits of The Register at The Entrance to The Xor Element
-		.BYTES_RVRS ('0  ), // Input Word Byte Reverse
-		.XOR_VECTOR ('0  ), // CRC Final Xor Vector
-		.NUM_STAGES (2   )  // Number of Register Stages, Equivalent Latency in Module. Minimum is 1, Maximum is 3.
+        .POLY_WIDTH (G_BIT_WIDTH   ),   // Size of The Polynomial Vector
+		.WORD_WIDTH (G_BIT_WIDTH   ),   // Size of The Input Words Vector
+		.WORD_COUNT (0   ),             // Number of Words To Calculate CRC, 0 - Always Calculate CRC On Every Input Word
+		.POLYNOMIAL ('hD5),             // Polynomial Bit Vector
+		.INIT_VALUE ('1  ),             // Initial Value
+		.CRC_REF_IN ('0  ),             // Beginning and Direction of Calculations: 0 - Starting With MSB-First; 1 - Starting With LSB-First
+		.CRC_REFOUT ('0  ),             // Determines Whether The Inverted Order of The Bits of The Register at The Entrance to The Xor Element
+		.BYTES_RVRS ('0  ),             // Input Word Byte Reverse
+		.XOR_VECTOR ('0  ),             // CRC Final Xor Vector
+		.NUM_STAGES (2   )              // Number of Register Stages, Equivalent Latency in Module. Minimum is 1, Maximum is 3.
     ) CRC (
-        .i_crc_a_clk_p (i_clk  ), // Rising Edge Clock
-		.i_crc_s_rst_p (m_axis.tready && m_axis.tvalid && q_crnt_state == S3), // Sync Reset, Active High. Reset CRC To Initial Value.
-		.i_crc_ini_vld ('0     ), // Input Initial Valid
-		.i_crc_ini_dat ('0     ), // Input Initial Value
-		.i_crc_wrd_vld ((m_axis.tready && m_axis.tvalid)), // Word Data Valid Flag 
-		.o_crc_wrd_rdy (/* Nothing bcs source don't output ready*/), // Ready To Recieve Word Data
-		.i_crc_wrd_dat (m_axis.tdata), // Word Data
-		.o_crc_res_vld (m_crc_valid), // Output Flag of Validity, Active High for Each WORD_COUNT Number
-		.o_crc_res_dat (m_crc_data )  // Output CRC from Each Input Word
+        .i_crc_a_clk_p (i_clk  ),       // Rising Edge Clock
+		.i_crc_s_rst_p (m_axis.tready && m_axis.tvalid && q_crnt_state == S3),  // Sync Reset, Active High. Reset CRC To Initial Value.
+		.i_crc_ini_vld ('0     ),       // Input Initial Valid
+		.i_crc_ini_dat ('0     ),       // Input Initial Value
+		.i_crc_wrd_vld ((m_axis.tready && m_axis.tvalid) && m_axis.tdata != 72),  // Word Data Valid Flag 
+		.o_crc_wrd_rdy (/* Nothing bcs source don't output ready*/),    // Ready To Recieve Word Data
+		.i_crc_wrd_dat (m_axis.tdata),  // Word Data
+		.o_crc_res_vld (m_crc_valid),   // Output Flag of Validity, Active High for Each WORD_COUNT Number
+		.o_crc_res_dat (m_crc_data )    // Output CRC from Each Input Word
     );
     
 endmodule
